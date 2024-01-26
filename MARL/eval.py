a6e5e1257a5690c2c80d36dc1a72d50ff1e938ca
Paul Auerbach <paul.auerbach@barkhauseninstitut.org>
2024-01-26 15:50:46 +0100

    Added visualization of the agents actions to the renderer

--- MARL/eval.py
+++ MARL/eval.py
@@ -1,6 +1,7 @@
 import time
 import gymnasium as gym
 
+import pygame
 import argparse
 import sys
 import os
@@ -8,6 +9,11 @@ import numpy as np
 from tqdm import tqdm
 import warnings
 warnings.filterwarnings('ignore')
+
+import matplotlib.cm as cm
+import matplotlib as mpl
+
+from PIL import Image
 
 sys.path.append("../highway-env")
 import highway_env
@@ -35,7 +41,45 @@ def parse_args():
     args = parser.parse_args()
     return args
 
+last_action_prob = None
+
+def display_action(action_surface, sim_surface):
+    cell_size = [action_surface.get_width()/5, 300]
+
+    action_map = [
+        'LANE_LEFT',
+        'IDLE',
+        'LANE_RIGHT',
+        'FASTER',
+        'SLOWER'
+        ]
+
+    global last_action_prob
+    if last_action_prob is None:
+        return
+
+    for i, value in np.ndenumerate(last_action_prob):
+        i = i[1]
+        cmap = cm.plasma
+        norm = mpl.colors.Normalize(vmin=0, vmax=1)
+        color = cmap(norm(value), bytes=True)
+        pygame.draw.rect(action_surface, color, (cell_size[0]*i, 0, cell_size[0], cell_size[1]), 0)
+
+        font = pygame.font.Font(None, 15)
+        # probilities
+        text = "p={:.2f}".format(value)
+        text = font.render(text,
+                          1, (10, 10, 10), (255, 255, 255))
+        action_surface.blit(text, (cell_size[0]*i, 0))
+
+        #action text
+        text = f"{action_map[i]}"
+        text = font.render(text,
+                          1, (10, 10, 10), (255, 255, 255))
+        action_surface.blit(text, (cell_size[0]*i, 20))
+
 def eval_policy(args):
+    # env = gym.make('merge-single-agent-v0', render_mode='rgb_array')
     env = gym.make('merge-single-agent-v0')
 
     env.config["screen_height"] = 300
@@ -64,13 +108,14 @@ def eval_policy(args):
     j = 0
     t = tqdm(range(num_tries))
     start = time.time()
-    # import cProfile, pstats
-    # profiler = cProfile.Profile()
-    # profiler.enable()
 
     for i in t:
       done = truncated = False
       obs, info = env.reset()
+      #set the envviewr in the env
+      env.render()
+      env.viewer.set_agent_display(display_action)
+      skip_run = False
 
       if args.initial_pos != '':
           load_veh = np.load(args.initial_pos, allow_pickle=True)
@@ -83,11 +128,15 @@ def eval_policy(args):
       while not (done or truncated):
         if not args.mobil:
             action, _states = model.predict(obs, deterministic=True)
+            t_obs = torch.tensor(obs)
+            t_obs = t_obs[None, :]
+            action_prob, action_log_prob = model.policy.actor.action_log_prob(t_obs)
+            global last_action_prob
+            last_action_prob = action_prob.detach().numpy()
         else:
             action = None
         obs, reward, done, truncated, info = env.step(action)
         ret += reward
-        # print(obs)
 
         speed += info["average_speed"]
         road_speed += info["average_road_speed"]
@@ -101,23 +150,34 @@ def eval_policy(args):
             env.render()
             time.sleep(0.1)
 
-      if info["other_crashes"]:
-          # print("crash happened")
-          other_crashes += 1
+        # also end the episode when another vehicle crashed
+        #if info["other_crashes"] and not info["crashed"]:
+            # frame = env.render()
+            # im = Image.fromarray(frame)
+            # im.save(f"crash_{i}.png")
+            #skip_run = True
+            #print("other crash")
+
+      #if info['crashed']:
+      # if info["other_crashes"]:
+      if skip_run:
+          # only save trajectories if we didn't load any in the first place
+          if args.initial_pos == '':
+              np.save(f"initial_pos_{i}.npy", env.road.initial_vehicles)
+
+      if skip_run:
+        continue
+
+      #if info["other_crashes"]:
+          #other_crashes += 1
 
       if info['crashed']:
           crashes += 1
 
-      if info['merged']:
-          sucessfull_merges += 1
+      #if info['merged']: # and not info["other_crashes"]:
+          #sucessfull_merges += 1
 
       j += 1
-
-      if info['crashed']:
-      # if info["other_crashes"]:
-          # only save trajectories if we didn't load any in the first place
-          if args.initial_pos == '':
-              np.save(f"initial_pos_{i}.npy", env.road.initial_vehicles)
 
       # if args.traj_dir != '':
           # np.save(f"{args.traj_dir}/pos_{i}.npy", np.array(position_list))
@@ -125,11 +185,8 @@ def eval_policy(args):
       # print(f"Episode done crashed:{info['crashed']}")
       # print(f"Current crashrate {crashes/(i+1)}")
       # t.set_description(f"Crashrate {crashes/(i+1)} Other crashes {other_crashes/(i+1)}")
+      # t.set_description(f"Crashrate {crashes/(i+1)} Mergerate {sucessfull_merges/(i+1-other_crashes)}")
       t.set_description(f"Crashrate {crashes/(i+1)} Mergerate {sucessfull_merges/(i+1)}")
-
-    # profiler.disable()
-    # stats = pstats.Stats(profiler)
-    # stats.dump_stats('eval_new.log')
 
     end = time.time()
     print(f"Took {(end-start)/j}")
