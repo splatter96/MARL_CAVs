c2a412a42b8f9f27d47d6eef75e39dc0c0612ee2
Paul Auerbach <paul.auerbach@barkhauseninstitut.org>
2024-09-05 11:54:13 +0200

    Modified the visualization of the trajectories to only include points for the last positions

--- MARL/plot_trajectory.py
+++ MARL/plot_trajectory.py
@@ -19,24 +19,27 @@ if __name__ == "__main__":
 
     sns.set_theme()
 
+    # plot_until = 15 #17 #21 #29 #near miss
+    plot_until = 19 #15 #17  #crash
+
     runs_list = []
     for f in glob.iglob(f"{args.runs}/pos_1.npy", recursive=True):
     # for f in glob.iglob(f"{args.runs}/pos_3.npy", recursive=True):
     # for f in glob.iglob(f"{args.runs}/pos_4.npy", recursive=True):
         data = np.load(f"{f}")
-        df = pd.DataFrame(data, columns=["x", "y"])
-        ax = sns.lineplot(data=df, x="x", y="y", marker="D")
+        df = pd.DataFrame(data[:plot_until, :], columns=["x", "y"])
+        ax = sns.lineplot(data=df, x="x", y="y", marker="D", markevery=[-1])
 
     for f in glob.iglob(f"{args.runs}/other_pos_1.npy", recursive=True):
     # for f in glob.iglob(f"{args.runs}/other_pos_3.npy", recursive=True):
     # for f in glob.iglob(f"{args.runs}/other_pos_4.npy", recursive=True):
         data = np.load(f"{f}")
         for i in range(data.shape[1]):
-            if i in [5,6,8]: #for run 1, crash
-            # if i in [9]: #for run 3, near miss
+            if i in [5,6]: #for run 1, crash
+            # if i in [9, 3]: #for run 3, near miss
             # if i in [4,6]: #for run 4, nice merge
-                df = pd.DataFrame(data[:,i,:], columns=["x", "y"])
-                ax = sns.lineplot(data=df, x="x", y="y", marker="o")
+                df = pd.DataFrame(data[:plot_until,i,:], columns=["x", "y"])
+                ax = sns.lineplot(data=df, x="x", y="y", marker="o", markevery=[-1])
 
     ax.invert_yaxis()
     ax.grid(False)
@@ -49,10 +52,12 @@ if __name__ == "__main__":
               extent = [150,460,16,-2],
               zorder = 0) #put the map under the plot
 
-    ax.set_xlim([225,300])
+    # ax.set_xlim([225,300])
 
     # matplotlib.pyplot.show()
     plt.tight_layout()
-    plt.savefig("trajectory_crash.png", dpi=600, bbox_inches="tight")
+    # plt.savefig("trajectory_crash.png", dpi=600, bbox_inches="tight")
     # plt.savefig("trajectory_near_miss.png", dpi=600, bbox_inches="tight")
     # plt.savefig("trajectory_nice_merge.png", dpi=600, bbox_inches="tight")
+
+    plt.savefig("trajectory_crash_2.png", dpi=600, bbox_inches="tight")
