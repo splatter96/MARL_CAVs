Fixed distance calculation on circular roads, made circular road a parameter of each lane

highway-env/highway_env/road/lane.py
@@ -168,7 +168,7 @@ class CommonRoadLane(AbstractLane):
     def __init__(
         self,
         lanelet: Lanelet,
-        ring: bool = False,
+        is_ring: bool = False,
         width: float = DEFAULT_WIDTH,
         line_types: Tuple[LineType, LineType] = None,
         forbidden: bool = False,
@@ -186,7 +186,7 @@ class CommonRoadLane(AbstractLane):
         :param priority: priority level of the lane, for determining who has right of way
         """
         self.lanelet = lanelet
-        self.ring = ring
+        self.ring = is_ring
         self.width = np.linalg.norm(
             self.lanelet.right_vertices[0] - self.lanelet.left_vertices[0]
         )
@@ -196,24 +196,25 @@ class CommonRoadLane(AbstractLane):
         self.line_types = line_types or [LineType.CONTINUOUS, LineType.CONTINUOUS]
         self.length = self.lanelet.distance[-1]
 
-        # resample center_vertices for equidistance and close spacing
-        # the spacing from the scenario designer is sometimes too corse
-        center_vertices = self.lanelet.center_vertices
+        if not self.ring:
+            # resample center_vertices for equidistance and close spacing
+            # the spacing from the scenario designer is sometimes too corse
+            center_vertices = self.lanelet.center_vertices
 
-        x_resample = np.linspace(center_vertices[0, 0], center_vertices[-1, 0], 100)
-        y_resample = np.interp(
-            x_resample,
-            center_vertices[:, 0],
-            center_vertices[:, 1],
-        )
-        resampled_vertices = np.column_stack([x_resample, y_resample])
-        self.lanelet.center_vertices = resampled_vertices
+            x_resample = np.linspace(center_vertices[0, 0], center_vertices[-1, 0], 100)
+            y_resample = np.interp(
+                x_resample,
+                center_vertices[:, 0],
+                center_vertices[:, 1],
+            )
+            resampled_vertices = np.column_stack([x_resample, y_resample])
+            self.lanelet.center_vertices = resampled_vertices
 
-        # need to recalculate the distance array after resampling
-        # thats usuallya called from inside the lanelet constructor
-        self.lanelet._distance = self.lanelet._compute_polyline_cumsum_dist(
-            [self.lanelet.center_vertices]
-        )
+            # need to recalculate the distance array after resampling
+            # thats usuallya called from inside the lanelet constructor
+            self.lanelet._distance = self.lanelet._compute_polyline_cumsum_dist(
+                [self.lanelet.center_vertices]
+            )
 
         # used for faster distance calculation in nearest neighbor search
         # argmin_a (a - b)^2 = argmin_a a^2 - 2ab + b^2 = argmin_a a^2 - 2ab  # noqa
@@ -330,12 +331,11 @@ class CommonRoadLane(AbstractLane):
         dx = x2 - x1
 
         if self.ring:
+            sign = -1 if dx < 0 else 1
             dx = abs(dx)
 
-            sign = (x2 - x1) / dx
-
             if dx > 0.5 * self.length:
-                dx = 1.0 - dx
+                dx = self.length - dx
                 sign = sign * -1
 
             return sign * dx

path_planner_vehicle.py
@@ -25,9 +25,7 @@ from highway_env.vehicle.graphics import VehicleGraphics
 
 import cProfile, pstats
 
-# file_path = "./ZAM_Test2Circ-1_1_T-1.xml"
 # file_path = "./DEU_MyTrack2LaneCont-1_1_T-1.xml"
-# file_path = "./DEU_HighwayMergeCombinedLanes-1_1_T-1.xml"
 file_path = "./DEU_HighwayMergeConnected2-1_1_T-1.xml"
 
 
@@ -146,137 +144,23 @@ class PathPlanner:
         # stats.dump_stats("profile_commonroad_views.log")
 
     def make_road(self):
-        # lane = StraightLane([0, 0], [3.5, 0], line_types=(LineType.CONTINUOUS_LINE, LineType.CONTINUOUS_LINE), width=0.3)
-        # lane_o = StraightLane([0, -0.3], [3.5, -0.3], line_types=(LineType.CONTINUOUS_LINE, LineType.CONTINUOUS_LINE), width=0.3)
-
-        # lane2 = CircularLane(center=[3.4, 1], radius=1, start_phase=-np.pi/2, end_phase=np.pi/2, width=0.3)
-        # lane2_o = CircularLane(center=[3.4, 1], radius=1.3, start_phase=-np.pi/2, end_phase=np.pi/2, width=0.3)
-
-        # lane3 = StraightLane([3.5, 2.0], [0, 2.0], line_types=(LineType.CONTINUOUS_LINE, LineType.CONTINUOUS_LINE), width=0.3)
-        # lane3_o = StraightLane([3.5, 2.3], [0.0, 2.3], line_types=(LineType.CONTINUOUS_LINE, LineType.CONTINUOUS_LINE), width=0.3)
-
-        # lane4 = CircularLane(center=[0.0, 1.0], radius=1.0, start_phase=np.pi/2, end_phase=1.5*np.pi, width=0.3)
-        # lane4_o = CircularLane(center=[0.0, 1.0], radius=1.3, start_phase=np.pi/2, end_phase=1.5*np.pi, width=0.3)
-        #
-
-        scenario, planning_problem_set = CommonRoadFileReader(file_path).open()
+        scenario, _ = CommonRoadFileReader(file_path).open()
 
         net = scenario.lanelet_network
-        # print(len(net.lanelets))
-        #
-        net_common_road = RoadNetworkCommonRoad(net)
-
-        # l = net_common_road.get_lane(1234)
-        # print(l)
-
-        # point = l.local_coordinates(np.array([-0.1, -0.0]))
-        # # point = l.local_coordinates(np.array([1.7, -1.0]))
-        # print(point)
-
-        # test_point = l.position(2.56, 2.0)
-        # print(f"testpoint {test_point}")
-
-        # local_point = l.position(1.8, -0.1)
-        # local_point = l.position(point[0], point[1])
-        # print(local_point)
-        #
-        # right_corner = l.local_coordinates(np.array([1.7, -1]))
-        # heading = l.heading_at(point[0])
-        # heading = l.heading_at(7.6)
-        # print(np.rad2deg(heading))
-        #
-        # exit(0)
-
-        straight_length = 0.8
-
-        lane = StraightLane(
-            [0, 0],
-            [straight_length, 0],
-            line_types=(LineType.CONTINUOUS_LINE, LineType.CONTINUOUS_LINE),
-            width=0.3,
-        )
-        lane_o = StraightLane(
-            [0, -0.3],
-            [straight_length, -0.3],
-            line_types=(LineType.CONTINUOUS_LINE, LineType.CONTINUOUS_LINE),
-            width=0.3,
-        )
-
-        lane2 = CircularLane(
-            center=[straight_length - 0.1, 1],
-            radius=1,
-            start_phase=-np.pi / 2,
-            end_phase=np.pi / 2,
-            width=0.3,
-        )
-        lane2_o = CircularLane(
-            center=[straight_length - 0.1, 1],
-            radius=1.3,
-            start_phase=-np.pi / 2,
-            end_phase=np.pi / 2,
-            width=0.3,
-        )
-
-        lane3 = StraightLane(
-            [straight_length, 2.0],
-            [0, 2.0],
-            line_types=(LineType.CONTINUOUS_LINE, LineType.CONTINUOUS_LINE),
-            width=0.3,
-        )
-        lane3_o = StraightLane(
-            [straight_length, 2.3],
-            [0.0, 2.3],
-            line_types=(LineType.CONTINUOUS_LINE, LineType.CONTINUOUS_LINE),
-            width=0.3,
-        )
-
-        lane4 = CircularLane(
-            center=[0.0, 1.0],
-            radius=1.0,
-            start_phase=np.pi / 2,
-            end_phase=1.5 * np.pi,
-            width=0.3,
-        )
-        lane4_o = CircularLane(
-            center=[0.0, 1.0],
-            radius=1.3,
-            start_phase=np.pi / 2,
-            end_phase=1.5 * np.pi,
-            width=0.3,
-        )
-
-        net = RoadNetwork()
-        net.add_lane("a", "b", lane)
-        net.add_lane("a", "b", lane_o)
-
-        net.add_lane("b", "c", lane2)
-        net.add_lane("b", "c", lane2_o)
-
-        net.add_lane("c", "d", lane3)
-        net.add_lane("c", "d", lane3_o)
-
-        net.add_lane("d", "a", lane4)
-        net.add_lane("d", "a", lane4_o)
+        net_common_road = RoadNetworkCommonRoad(net, is_ring=False)
 
         self.road = RoadCommonRoad(network=net_common_road)
         merge_obstacle = Obstacle(self.road, np.array([310, -4]))
         merge_obstacle.lane_index = 7146164179188
         # self.road.objects.append(merge_obstacle)
 
-    def oldmake_vehicles(self):
+    def othermake_vehicles(self):
         for car in self.virtual_car_configs:
             id = car["id"]
-            # v = ModelIDMVehicle(
-            #     self.road,
-            #     np.array([0.5 * id, 0], dtype=np.float64),
-            #     speed=car["initial_speed"],
-            # )
-            # v = ModelIDMVehicle.make_on_lane(
-            v = IDMVehicle.make_on_lane(
+            v = ModelIDMVehicle.make_on_lane(
                 self.road,
                 car["initial_lane_index"],
-                # 0.5 * id,
-                10 * id + 1,
+                0.5 * id,
                 car["initial_speed"],
             )
             v.id = id