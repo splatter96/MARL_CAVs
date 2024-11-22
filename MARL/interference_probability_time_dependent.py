Fixed bug in time calculation for interference calculation

MARL/interference_probability_time_dependent.py
@@ -2,15 +2,15 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 simulation_frequency = 15  # [Hz] frequency of the main traffic simulation
-radar_frequency = 500  # [Hz] frequency of radar overlap calculation
+radar_frequency = 2000  # [Hz] frequency of radar overlap calculation
 
-# frame_time = (
-#     20.0 / 1000.0
-# )  # [s] total time of a singel radar frame including on and of time
+frame_times = [
+    20.0 / 1000,
+    9.0 / 1000,
+    20.0 / 1000,
+]  # [s] duration of a single radar frame
 
-frame_times = [20.0 / 1000, 9.0 / 1000, 20.0 / 1000]
-
-duty_cycles = [0.03, 0.03, 0.03]  # [% / 100]
+duty_cycles = [0.06, 0.03, 0.03]  # [% / 100]
 
 cycle_offsets = [
     0 / 1000,
@@ -18,12 +18,14 @@ cycle_offsets = [
     10 / 1000,
 ]  # [s] offset from start of simulation of the first on period of the radar
 
-
 end_time = 10  # [s]
 
 radar_steps_per_frame = int(frame_times[0] * radar_frequency)
 timestep = 1 / simulation_frequency
 radar_frames_per_timestep = int(timestep / frame_times[0])
+
+print(f"Radar steps per radar frame {radar_steps_per_frame}")
+print(f"Minimal possible duty cycle {frame_times[0]/radar_steps_per_frame}")
 
 
 def is_on(t, duty_cycle, offset, frame_time):
@@ -37,6 +39,7 @@ overlap = False
 t = 0
 while t < end_time:
     for _ in range(radar_frames_per_timestep):
+        t2 = t
         for _ in range(radar_steps_per_frame):
             for i in range(len(duty_cycles) - 1):
                 if is_on(t, duty_cycles[0], cycle_offsets[0], frame_times[0]) and is_on(
@@ -49,18 +52,16 @@ while t < end_time:
                 overlap = False
                 break
 
-            t += 1 / radar_frequency
+            t2 += 1 / radar_frequency
 
         frames += 1
     t += timestep
-    print(f"Time {t}")
 
 print(f"Calculated {frames}")
 print(f"Overlaps prob {overlaps/(frames)}")
 
 
-T = np.linspace(0, 0.05, 10000)
-# T = np.linspace(0, end_time, 10000)
+T = np.linspace(0, 0.2, 10000)
 plt.plot(T, is_on(T, duty_cycles[0], cycle_offsets[0], frame_times[0]), label="ego")
 
 for i in range(len(duty_cycles) - 1):